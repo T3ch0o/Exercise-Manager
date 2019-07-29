d9f12972560d4774b2093eaf6c293073fb596c07
TSM Techø <trottimusbg@gmail.com>
2019-07-29 13:55:09 +0300

    [MainViewModel] Add Tab items.

--- Exercise Manager/ViewModels/MainViewModel.cs
+++ Exercise Manager/ViewModels/MainViewModel.cs
@@ -1,8 +1,15 @@
 namespace Exercise.Manager.ViewModels
 {
+    using Caliburn.Micro;
     using Exercise.Manager.ViewModels.Interfaces;
 
-    public class MainViewModel : ViewModelBase, IMainViewModel
+    public class MainViewModel : Conductor<IViewModelBase>.Collection.OneActive, IMainViewModel
     {
+        public MainViewModel(IDashboardViewModel dashboard,
+            IExercisesViewModel exercises)
+        {
+            Items.Add(dashboard);
+            Items.Add(exercises);
+        }
     }
 }
