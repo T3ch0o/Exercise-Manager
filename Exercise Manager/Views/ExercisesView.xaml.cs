be06d2aef755f16a5be7e5e4ffb6d7fd6ce513c8
TSM Techø <trottimusbg@gmail.com>
2019-07-29 13:53:29 +0300

    [ExercisesView]

---
+++ Exercise Manager/Views/ExercisesView.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Exercise.Manager.Views
+{
+    public partial class ExercisesView
+    {
+        public ExercisesView()
+        {
+            InitializeComponent();
+        }
+    }
+}
